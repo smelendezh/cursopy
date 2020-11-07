dbfc0d041f462b1cd4ffadea27c5096ea2d39e76
Sigifredo Melendez H <smelendezh@gmail.com>
2020-11-07 09:18:16 -0500

    juego

--- juego.py
+++ juego.py
@@ -3,6 +3,6 @@ import random
 numrand=random.randint(0,5)
 num=-1
 while num != numrand:
-    num=int(input("Que número estoy pensando??: "))
+    num=int(input("Que número estoy pensando?: "))
 
-print("Adivinaste era el número %d" % (int(num)))
+print("Adivinaste era el número que pensaba %d" % (int(num)))
